125b47bbf893108dd83d23098bde51e528c154e5
sep3r <sep3r@yahoo.com>
2026-09-01 21:25:59 +0100

    fix(backend): fix security for vercel

--- backend/src/main/java/com/monikoshop/config/SecurityConfig.java
+++ backend/src/main/java/com/monikoshop/config/SecurityConfig.java
@@ -57,7 +57,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "http://frontend"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000",
+                "http://localhost:5173",
+                "https://moniko-shop-flax.vercel.app",
+                "http://frontend"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
